41d6cf3585cfeb222f329d4658e4d9b14f230a29
Samuel Bitdinger <samuel.bitdinger@gmail.com>
2024-05-16 10:54:20 -0300

    ajustes na pagina das casas

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //Galeria de Fotos
-
-console.log("oi");
-
 const fotinhas = document.querySelectorAll('.foto-pequena');
-
 fotinhas.forEach(teste);
-
-
 function teste(fotinha) {
   fotinha.addEventListener("click", () => {
     const fotos = document.querySelectorAll('.foto-grande');
     const id = fotinha.getAttribute('id');
+    fotinhas.forEach((item) => {
+      if (item.classList.contains('selecionada')) {
+        item.classList.remove('selecionada');
+      }
+    })
+    fotinha.classList.add('selecionada');
     for (var i = 0; i < 4; i++) {
       if (fotos[i].getAttribute('id') === id) {
         if (fotos[i].classList.contains('ativa') === false) {
@@ -25,5 +25,3 @@ function teste(fotinha) {
     }
   })
 }
-
-

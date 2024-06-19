33cc29a4320d4402798fa0be6cb8bbf06582ebba
Samuel Bitdinger <samuel.bitdinger@gmail.com>
2024-06-19 16:42:25 -0300

    finalizando a animação na galeria de fotos

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
 //Galeria de Fotos
 const fotosPequenas = document.querySelectorAll('.foto-pequena');
 const fotosGrandes = document.querySelectorAll('.foto-grande');
+
 fotosPequenas.forEach(trocaFoto);
+
 function trocaFoto(fotoClicada) {
 	fotoClicada.addEventListener("click", () => {
-		const id = fotoClicada.getAttribute('id'); // id da foto clicada
+		const id = fotoClicada.getAttribute('id'); // id da foto clicada\
 
-		//arrumando as fotos clicaveis
-		fotosPequenas.forEach((item) => {
-			if (item.classList.contains('selecionada')) {
-				item.classList.remove('selecionada');
-			}
-		})
-		fotoClicada.classList.add('selecionada');
+		arrumaOpacidade(fotoClicada);//arrumando as fotos clicaveis 
 
-		fotosGrandes.forEach((foto) => {
-			if (foto.getAttribute('id') === id) {
-				//aparição da foto clicada
-				if (foto.classList.contains('ativa') === false) {
-					foto.classList.add('ativa');
-					foto.classList.remove('inativa');
+		let proximaFoto = document.querySelector(`.foto-grande[id="${id}"]`);
+		let fotoAtual = document.querySelector('.foto-grande.ativa');
 
-					//animaçao de saida da foto
-					if (foto.classList.contains('ativa')) {
-						foto.classList.add('saindo');
-						foto.addEventListener('animationend', () => {
-							foto.classList.remove('saindo');
-						})
-					}
-				}
-			}
-			//remoção da foto anterior
-			if (foto.getAttribute('id') != id) {
-				foto.classList.remove('ativa');
-				foto.classList.add('inativa');
-			}
-		})
+		if (fotoAtual.getAttribute('id') != id) {
+			fotoAtual.classList.add('saindo');
+			fotoAtual.addEventListener('animationend', () => {
+				fotoAtual.classList.add('inativa');
+				fotoAtual.classList.remove('saindo', 'ativa');
+				proximaFoto.classList.add('ativa', 'entrando');
+				proximaFoto.classList.remove('inativa');
+			}, { once: true })
+		}
+
+
 	})
 }
 
+function arrumaOpacidade(fotoClicada) {
+	fotosPequenas.forEach((item) => {
+		if (item.classList.contains('selecionada')) {
+			item.classList.remove('selecionada');
+		}
+	})
+	fotoClicada.classList.add('selecionada');
+}
